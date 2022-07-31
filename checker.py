5917c975abc599864529803a5703a95dff45883f
Amirsam Goharpey <50692220+amirsamgoharpay@users.noreply.github.com>
2022-07-31 09:31:45 -0700

    create codes and check them

--- checker.py
+++ checker.py
@@ -7,24 +7,29 @@ check = True
 code = ""
 cho = 0
 while check:
-        for i in range(16):
-                code =code + random.choice(chars)
-        res = requests.get(f"https://discord.com/api/v9/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true")
-        res = res.json()
-        #if res["message"] == "The resource is being rate limited." :
-                #print("this proses after 1 min")
-        if res["message"] == "Unknown Gift Code" :
-                print(f"{code} was invalid")
-        else :
-                print(res["message"])
-                print(code)
-        dic = {"message" : res["message"],
+        try:
+                for i in range(16):
+                        code =code + random.choice(chars)
+                res = requests.get(f"https://discord.com/api/v9/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true")
+                res = res.json()
+                #if res["message"] == "The resource is being rate limited." :
+                        #print("this proses after 1 min")
+                if res["message"] == "Unknown Gift Code" :
+                        print(f"{code} was invalid")
+                else :
+                        print(res["message"])
+                        print(code)
+                dic = {"message" : res["message"],
                 "code" : code}
-        with open("codes.txt" ,"a") as f:
-                f.write(f"{dic}\n")
-                #json.dump(dic , f)
-        time.sleep(12)
+                with open("codes.txt" ,"a") as f:
+                        f.write(f"{dic}\n")
+                        #json.dump(dic , f)
+                code = ""
+                time.sleep(12)
+        except KeyboardInterrupt as error :
+                print("codes are saved in codes.txt")
+                exit()
 
 
 
-        code = ""
+
