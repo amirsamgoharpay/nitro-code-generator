33360e2c02230c0f3e15d1b1eb7f16aa957360c4
Amirsam Goharpey <50692220+amirsamgoharpay@users.noreply.github.com>
2022-07-31 09:29:44 -0700

    Update creator.py

--- creator.py
+++ creator.py
@@ -4,14 +4,16 @@ chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz"
 check = True
 code = ""
 cho = 0
+print("starting...")
 while check:
-        for i in range(16):
-                code =code + random.choice(chars)
-        print(code)
-        with open("codes.txt" ,"a") as f:
-                f.write(f"{code}\n")
-        time.sleep(0.001) 
+        try:
+                for i in range(16):
+                        code =code + random.choice(chars)
+                #print(code) this line prints the codes that are generated (its kinda annoying)
+                with open("code.txt" ,"a") as f:
+                        f.write(f"{code}\n")
+                time.sleep(0.001) 
+                code = ""
+        except KeyboardInterrupt as error :
+                print(" codes are saved in code.txt")
 
-
-
-        code = ""
