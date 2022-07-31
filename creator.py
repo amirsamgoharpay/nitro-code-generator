3ed49c93239bda628943415758ef7c6dc3507d8a
Amirsam Goharpey <50692220+amirsamgoharpay@users.noreply.github.com>
2022-07-31 09:21:03 -0700

    only generate the code

---
+++ creator.py
@@ -0,0 +1,19 @@
+import requests
+import random
+import json
+import time
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz"
+check = True
+code = ""
+cho = 0
+while check:
+        for i in range(16):
+                code =code + random.choice(chars)
+        print(code)
+        with open("codes.txt" ,"a") as f:
+                f.write(f"{code}\n")
+        time.sleep(0.001) 
+
+
+
+        code = ""
